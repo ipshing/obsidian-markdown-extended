3f9868a07b1dd8f8659b46c500526d9757089660
ipshing <ipshing@outlook.com>
2024-03-25 23:09:42 -0700

    Make inline render less destructive

--- src/components/list.ts
+++ src/components/list.ts
@@ -127,64 +127,117 @@ export function renderDescriptionList(container: HTMLElement) {
 
 export function renderInlineDescriptionList(container: HTMLElement) {
     // Get all paragraph tags (<p>) in the container
-    container.findAll("p").forEach((paragraph) => {
+    const paragraphs = container.findAll("p");
+    for (const paragraph of paragraphs) {
+        // Skip paragraphs that don't have the token
+        if (!paragraph.textContent.contains(DLIST_INLINE_TOKEN)) continue;
+
         // Get the parent of 'paragraph'
         const parent = paragraph.parentElement;
-        // Set up 'lastEl' as a reference for where to insert new elements
+        // Set up lastEl as a reference for where to insert new elements
         let lastEl = paragraph;
-        // Use innerHTML to split the text up by line breaks (<br>)
-        const lines = paragraph.innerHTML.split("<br>");
-        // Iterate through lines to build list(s). Any text not part of a list
-        // (before or after) should be put in <p> tags.
-        let pLines: string[] = [];
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i].trim();
-            // Only the first occurrence of "::" is important
-            // (surrounding spaces don't matter)
-            const index = line.indexOf(DLIST_INLINE_TOKEN);
-            // There needs to be at least one character before
-            // the first occurrence of the inline token
-            if (index > 0) {
-                // If there is any text in pLines, push that to
-                // a <p> tag and add to the container
-                if (pLines.length > 0) {
-                    const p = createEl("p");
-                    // Join using a line break (<br>) and new line char
-                    // (matches Obsidian behavior for multi-line text)
-                    p.innerHTML = pLines.join("<br>\n");
-                    // Insert after 'paragraph'
-                    parent.insertAfter(p, lastEl);
-                    lastEl = p;
-                    // Reset pLines
-                    pLines = [];
+        // Set up a temp paragraph to hold nodes while iterating
+        let temp = createEl("p");
+        // Set up lastBR as a way to mark split points in temp
+        let lastBR: HTMLBRElement;
+        // Delcare the description list elements outside of the loop
+        let dl: HTMLDListElement, dt: HTMLElement, dd: HTMLElement;
+        // Iterate using a while loop and the firstChild. Because we're
+        // appending the children to other collections, they get removed
+        // from 'paragraph' which messes with for loops.
+        while (paragraph.firstChild) {
+            const child = paragraph.firstChild;
+            // Line breaks (<br>) are the key dividers
+            if (child.nodeName == "BR") {
+                // Push the list if there is one
+                if (dl) {
+                    parent.insertAfter(dl, lastEl);
+                    lastEl = dl;
+                    // Reset all list items
+                    dl = null;
+                    dt = null;
+                    dd = null;
                 }
-                // Create a new list, split the line into the term/details,
-                // then add the list to the container.
-                const descriptionList = new DescriptionList();
-                descriptionList.items.push({
-                    term: line.slice(0, index).trim(),
-                    details: [line.slice(index + DLIST_INLINE_TOKEN.length).trim()],
-                });
-                const list = descriptionList.generateHtmlElement();
-                parent.insertAfter(list, lastEl);
-                lastEl = list;
+                // Add the line break to temp
+                temp.append(child);
+                // Track this <br> in lastBR. This will help with
+                // identifying what goes into the dl below.
+                lastBR = child as HTMLBRElement;
             }
-            // Otherwise, add the line to pLines
+            // If dl has a value, a list is being built;
+            // everything goes in the list
+            else if (dl) {
+                if (dd == null) {
+                    // This shouldn't happen, but just in case,
+                    // create a new dd and add it to dl.
+                    dd = dl.createEl("dd");
+                }
+                // Append child to dd
+                dd.append(child);
+            }
+            // #text nodes that contain the token are the indicator for a list
+            else if (child.nodeName == "#text" && child.textContent.contains(DLIST_INLINE_TOKEN)) {
+                // Create a new list with a dt and dd
+                dl = createEl("dl");
+                dt = dl.createEl("dt");
+                dd = dl.createEl("dd");
+
+                // Check for a <br> in temp. Everything before goes into a <p>
+                // in the parent. Everything after goes into the <dt>.
+                if (lastBR) {
+                    while (lastBR.nextSibling) {
+                        // Push to dt (automatically removes from temp)
+                        dt.append(lastBR.nextSibling);
+                    }
+                    // Remove the <br> from temp
+                    temp.removeChild(lastBR);
+                    // Reset lastBR
+                    lastBR = null;
+                    // Push temp to parent
+                    parent.insertAfter(temp, lastEl);
+                    lastEl = temp;
+                }
+                // Othewrise, put everything from temp into the <dt>
+                else {
+                    temp.childNodes.forEach((node) => {
+                        dt.append(node);
+                    });
+                }
+                // Reset temp
+                temp = createEl("p");
+
+                // Split the #text node around the token
+                const i = child.textContent.indexOf(DLIST_INLINE_TOKEN);
+                // Put everything before into dt
+                dt.append(child.textContent.slice(0, i).trim());
+                // Put everything after into dd
+                dd.append(child.textContent.slice(i + DLIST_INLINE_TOKEN.length).trim());
+                // Remove the child from paragraph
+                paragraph.removeChild(child);
+            }
+            // For now, everything that's not a #text element should not
+            // be checked. Just put it in temp until a token is found.
             else {
-                pLines.push(line.trim());
+                temp.append(child);
             }
         }
-        // If there any left-over lines of text, push those into
-        // a final <p> element.
-        if (pLines.length > 0) {
-            const p = createEl("p");
-            // Join using a line break (<br>) and new line char
-            // (matches Obsidian behavior for multi-line text)
-            p.innerHTML = pLines.join("<br>\n");
-            // Insert after 'paragraph'
-            parent.insertAfter(p, lastEl);
+
+        // If there is a list, push it
+        if (dl) {
+            parent.insertAfter(dl, lastEl);
+            lastEl = dl;
         }
-        // Finally, remove 'paragraph'
+        // If there is anything left in temp, push it
+        if (temp.hasChildNodes()) {
+            // If the first or last nodes are line breaks, remove them
+            if (temp.firstChild.nodeName == "BR") temp.removeChild(temp.firstChild);
+            if (temp.lastChild.nodeName == "BR") temp.removeChild(temp.lastChild);
+            // If there are still nodes left, push temp
+            if (temp.hasChildNodes()) {
+                parent.insertAfter(temp, lastEl);
+            }
+        }
+        // Finally, remove paragraph
         parent.removeChild(paragraph);
-    });
+    }
 }
